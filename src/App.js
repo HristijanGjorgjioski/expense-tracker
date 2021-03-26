5bf73fb645fef9723d62a13d26b9148da1893e4e
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-26 16:28:43 +0100

    Added background, Details and Main components and some styles for the components. I use @material-ui for styling!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Details from './components/Details/Details';
+
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
     return (
         <div>
-            App!
+            <Grid className={classes.grid} container spacing={0} alignItems="center" jusitfy="center" style={{ height: '100vh' }}>
+                <Grid item xs={12} sm={4}>
+                    <Details title="Income" />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details title="Expenses" />
+                </Grid>
+            </Grid>
         </div>
     )
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+
+import useStyles from './styles';
+
+function Main() {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+            <CardContent>
+                <Typography align="center" variant="h5">Total Balance $100</Typography>
+                <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>Try saying: Add incom for $100 in Category Salary for Monday</Typography>
+                <Devider />
+            </CardContent>
+            <CardContent className={classes.CardContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
