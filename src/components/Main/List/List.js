b71e7c243d2a05b19f371bbde77baf5247303375
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-26 19:17:21 +0100

    Initialize context and useContext in the app

--- src/components/Main/List/List.js
+++ src/components/Main/List/List.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
 
+import { ExpenseTrackerContext } from '../../../context/context';
 import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
+    const { deleteTransaction } = useContext(ExpenseTrackerContext);
 
     const transactions = [
         { id: 1, type: "Income", category: "Salary", amount: 50, date: "Fri Mar 26 2021" },
-        { id: 1, type: "Income", category: "PC", amount: 80, date: "Fri Mar 27 2021" },
-        { id: 1, type: "Expense", category: "Pets", amount: 120, date: "Fri Mar 28 2021" }
+        { id: 2, type: "Income", category: "PC", amount: 80, date: "Fri Mar 27 2021" },
+        { id: 3, type: "Expense", category: "Pets", amount: 120, date: "Fri Mar 28 2021" }
     ];
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
