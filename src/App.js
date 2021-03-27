3e8823d6a8b11e2c5080b293001496929d3d2e8f
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-27 16:29:08 +0100

    After more than 3-4 hours of coding and debugging, 'speechly' finally work with the app!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
 
 import Details from './components/Details/Details';
 import Main from './components/Main/Main';
@@ -21,6 +22,10 @@ const App = () => {
                     <Details title="Expense" />
                 </Grid>
             </Grid>
+            <PushToTalkButtonContainer>
+                <PushToTalkButton />
+                <ErrorPanel />
+            </PushToTalkButtonContainer>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>, 
+    <SpeechProvider appId="30f1e333-97bf-40a3-8bdd-c8e34c7610b5" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
     document.getElementById('root')
 );
